487aa14d7ef71af9e66a50bbb27782025a97f721
cwfly <1129367293@qq.com>
2020-05-31 20:23:05 +0800

    common-util

--- gmall-user/src/main/java/com/cw/gmall/user/controller/UserController.java
+++ gmall-user/src/main/java/com/cw/gmall/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.cw.gmall.user.controller;
 
+import com.cw.gmall.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
